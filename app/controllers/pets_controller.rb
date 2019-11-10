a0f3638d76ac7fcc73b897b74f52228f8bc505d2
Alyssa Yates <bosslys@icloud.com>
2019-11-10 22:08:16 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -24,6 +24,7 @@ class PetsController < ApplicationController
 
   get '/pets/:id' do
     @pet = Pet.find_by_id(params[:id])
+    @pet.owner = Owner.find_by_id(params[:id])
 
     erb :'/pets/show'
   end
