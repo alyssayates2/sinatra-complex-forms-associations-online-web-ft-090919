40f9dc2bdca9ee17f4e8ac45474fe4a76a1eaf29
Alyssa Yates <bosslys@icloud.com>
2019-11-10 22:09:47 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -24,26 +24,27 @@ class PetsController < ApplicationController
 
   get '/pets/:id' do
     @pet = Pet.find_by_id(params[:id])
+    @pet.owner = Owner.find_by_id(params[:id])
 
     erb :'/pets/show'
   end
 
   patch '/pets/:id' do
-    # if !params[:pet].keys.include?("owner_ids")
-    #   params[:pet]["owner_ids"] = []
-    # end
-    #
-    # @pet = Pet.find(params[:id])
-    # @pet.update(params["pet"])
-    # if !params["owner"]["name"].empty?
-    #   @pet.owners << Owner.create(name: params["owner"]["name"])
-    # end
+    if !params[:pet].keys.include?("owner_ids")
+      params[:pet]["owner_ids"] = []
+    end
+
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pet.owners << Owner.create(name: params["owner"]["name"])
+    end
     redirect "owners/#{@pet.id}"
   end
 
   get '/pets/:id/edit' do
-    # @owners = Owner.all
-    # @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    @pet = Pet.find(params[:id])
     redirect '/pets/edit'
   end
 
